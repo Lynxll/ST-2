b57dd251a7feae30f793b4de402e456c89493238
Lynxll <mrspider0530@gmail.com>
2026-05-30 19:46:38 +0300

    2

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,4 +1,41 @@
 // Copyright 2022 UNN-CS
-#include <cstdint>
+#include <cmath>
 #include "circle.h"
 
+namespace {
+const double PI = 3.14159265358979323846;
+}  // namespace
+
+Circle::Circle(double r) {
+  setRadius(r);
+}
+
+void Circle::setRadius(double r) {
+  radius = r;
+  ference = 2.0 * PI * radius;
+  area = PI * radius * radius;
+}
+
+void Circle::setFerence(double f) {
+  ference = f;
+  radius = ference / (2.0 * PI);
+  area = PI * radius * radius;
+}
+
+void Circle::setArea(double a) {
+  area = a;
+  radius = std::sqrt(area / PI);
+  ference = 2.0 * PI * radius;
+}
+
+double Circle::getRadius() const {
+  return radius;
+}
+
+double Circle::getFerence() const {
+  return ference;
+}
+
+double Circle::getArea() const {
+  return area;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 // Copyright 2022 UNN-CS
+#include <iostream>
 #include "circle.h"
+#include "tasks.h"
 
 int main() {
-    return 0;
+  std::cout << "Rope gap: " << solveRopeTask() << " m" << std::endl;
+  std::cout << "Pool cost: " << solvePoolTask() << " rub" << std::endl;
+  return 0;
 }
